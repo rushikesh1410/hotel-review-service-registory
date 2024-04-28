79955670f9908a68a37b159f6c55f6fce96add61
rushikesh1410 <rushikesh.shinde@cognizant.com>
2024-04-28 07:58:10 +0530

    Hotel review system service registory microservice

---
+++ src/test/java/com/lcwr/service/registory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwr.service.registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
